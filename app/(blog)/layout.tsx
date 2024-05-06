52141273a11f07ad0bdc0bd4dc4d764ad6602640
Samuel Silva <samuelsilva@Samuels-MacBook-Pro.local>
2024-05-06 15:21:06 -0400

    Upadting footer

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -74,14 +74,14 @@ async function Footer() {
         ) : (
           <div className="flex flex-col items-center py-28 lg:flex-row">
             <h3 className="mb-10 text-center text-4xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
-              Built with Next.js.
+              Built with Love
             </h3>
             <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
               <a
-                href="https://nextjs.org/docs"
+                href="https://linkeen.com"
                 className="mx-3 mb-6 border border-black bg-black py-3 px-12 font-bold text-white transition-colors duration-200 hover:bg-white hover:text-black lg:mb-0 lg:px-8"
               >
-                Read Documentation
+                Linkeen
               </a>
               <a
                 href="https://github.com/vercel/next.js/tree/canary/examples/cms-sanity"
