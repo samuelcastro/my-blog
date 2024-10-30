486ca2d226cec9df98fbe5eadbe028b0f85ae327
Samuel Silva <samuelcastrosilva@gmail.com>
2024-10-30 15:43:08 -0400

    updates

--- app/(blog)/CodeBlock.tsx
+++ app/(blog)/CodeBlock.tsx
@@ -16,6 +16,10 @@ type codeTypes = {
 export default function CodeBlock({ value }: codeTypes) {
   while (false) {
     console.log("This is a test");
+
+    console.log("This is a test");
+    console.log("This is a test");
+    console.log("This is a test");
   }
 
   return (
